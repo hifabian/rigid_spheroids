8bea8da3ccf0a2f6c5ce4f7ef4c91e0a3bc8220d
hifabian <49561468+hifabian@users.noreply.github.com>
2026-06-30 13:47:49 +0900

    Replace matrices with xz and yz shear
    To support yz shear, a symmetry is lost and negative m values for real
    spherical harmonics (sine terms) need to be included. This updates the
    indexing and matrix calculations for this change.

--- src/utils/idx.m
+++ src/utils/idx.m
@@ -1,8 +1,8 @@
 function i = idx(l,m,Lmax)
 % Map spherical harmonics indices to linearized index.
 % Linearized index is set to 0 if out of bounds.
-    i = 1+0.25*l*l+m;
-    if m > l || m < 0 || i > (1+(Lmax/2)^2+Lmax)
+    i = 1+0.5*l*(l+1)+m;
+    if m > l || m < -l || i > (1+Lmax*(Lmax+1)/2+Lmax)
         i = 0; % Out of bounds
     end
 end
