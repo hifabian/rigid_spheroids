478837e0fe8bca67404537a8185b55fb07e01188
hifabian <49561468+hifabian@users.noreply.github.com>
2026-07-03 16:11:55 +0900

    Test input validity for supported flow types
    This is only applied to constant Du as input, and not for time-dependent
    Du(t).

--- src/utils/flow_parameters.m
+++ src/utils/flow_parameters.m
@@ -5,7 +5,11 @@ function [gxz, wy, gyz, wx] = flow_parameters(Du)
         gyz = Du(3,2)+Du(2,3);
         wx  = Du(2,3);
 
-        % TODO warning if any other values are nonzero!
+        assert(Du(1,1) == 0, "Non-zero u_{x,x} not supported!")
+        assert(Du(1,2) == 0, "Non-zero u_{y,x} not supported!")
+        assert(Du(2,1) == 0, "Non-zero u_{x,y} not supported!")
+        assert(Du(2,2) == 0, "Non-zero u_{y,y} not supported!")
+        assert(Du(3,3) == 0, "Non-zero u_{z,z} not supported!")
     else
         gxz = @(Du) Du(3,1)+Du(1,3);
         wy  = @(Du) -Du(1,3);
